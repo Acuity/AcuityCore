fb40341ca730b187b7398e376dca07a0e7ba7396
Zachary Herridge <zgherridge@gmail.com>
2017-06-30 14:41:50 -0500

    Memory tests

---
+++ acuity-api/src/main/java/com/acuity/inject/test/Memory.java
@@ -0,0 +1,17 @@
+package com.acuity.inject.test;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Zachary Herridge on 6/30/2017.
+ */
+public class Memory {
+
+    public static GarbageCollectorMXBean gcBean;
+
+    public static List<GarbageCollectorMXBean> getBeans(){
+        return new ArrayList<>();
+    }
+}
