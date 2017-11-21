1f7629f944b610a80590be3370b486b689fff796
Zachary Herridge <zgherridge@gmail.com>
2017-11-21 08:01:31 -0600

    Expanded context menu api.

--- acuity-api/src/main/java/com/acuity/api/rs/wrappers/common/locations/screen/ScreenLocationShape.java
+++ acuity-api/src/main/java/com/acuity/api/rs/wrappers/common/locations/screen/ScreenLocationShape.java
@@ -1,11 +1,14 @@
 package com.acuity.api.rs.wrappers.common.locations.screen;
 
+import java.awt.*;
+
 /**
  * Created by Zachary Herridge on 7/10/2017.
  */
 public class ScreenLocationShape {
 
     private ScreenLocation[] locations;
+    private Polygon polygon;
 
     public ScreenLocationShape(ScreenLocation... locations) {
         this.locations = locations;
@@ -15,7 +18,17 @@ public class ScreenLocationShape {
         return locations;
     }
 
+    public Polygon getPolygon(){
+        if (polygon == null){
+            polygon = new Polygon();
+            for (ScreenLocation location : locations) {
+                polygon.addPoint(location.getX(), location.getY());
+            }
+        }
+        return polygon;
+    }
+
     public boolean contains(ScreenLocation location){
-        return false;
+        return getPolygon().contains(location.toPoint());
     }
 }
