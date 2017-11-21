20c9cbc043f9c4d91ec4cc920d0125fb49dfa6b5
Zachary Herridge <zgherridge@gmail.com>
2017-11-21 09:22:25 -0600

    Added angles.

--- acuity-api/src/main/java/com/acuity/api/rs/utils/Camera.java
+++ acuity-api/src/main/java/com/acuity/api/rs/utils/Camera.java
@@ -1,7 +1,11 @@
 package com.acuity.api.rs.utils;
 
 import com.acuity.api.AcuityInstance;
+import com.acuity.api.rs.interfaces.Locatable;
+import com.acuity.api.rs.wrappers.common.locations.WorldLocation;
 import com.acuity.api.rs.wrappers.common.locations.screen.ScreenLocation;
+
+import java.util.Optional;
 
 /**
  * Created by Zachary Herridge on 6/9/2017.
@@ -30,5 +34,21 @@ public class Camera {
 
     public static boolean isVisible(ScreenLocation screenLocation){
         return Projection.GAME_SCREEN.contains(screenLocation);
+    }
+
+    public static int getAngle() {
+        return (int) ((360D / 2048) * Math.min(2047 - getYaw(), 2048));
+    }
+
+    public static int getAngleTo(Locatable target) {
+        WorldLocation local = LocalPlayer.getWorldLocation().orElse(null);
+        WorldLocation other = Optional.ofNullable(target).map(Locatable::getWorldLocation).orElse(null);
+
+        if (local == null) throw new NullPointerException("Failed to load LocalPlayer.");
+        if (other == null) throw new NullPointerException("Failed to load target location..");
+
+        int angle = 90 - ((int) Math.toDegrees(Math.atan2(other.getWorldY() - local.getWorldY(), other.getWorldX() - local.getWorldX())));
+        if (angle < 0) angle += 360;
+        return angle % 360;
     }
 }
